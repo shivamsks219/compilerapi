12870b48727439e82cd1237326c38f59a40cf8aa
Shivam Kumar <68140446+shivamsks219@users.noreply.github.com>
2024-01-19 22:01:51 +0530

    Update main.py

--- main.py
+++ main.py
@@ -25,6 +25,6 @@ def run_code():
     except Exception as e:
         return jsonify({'error': str(e)}), 500
 
-if __name__ == '__main__':
-    app.run(debug=True)
+# if __name__ == '__main__':
+#     app.run(debug=True)
 
